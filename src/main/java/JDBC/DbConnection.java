3078ff7387ede1b607d2e83b571d69bacc05e921
salah <akilsalaheddine203@gmail.com>
2024-09-04 14:45:45 +0200

    README FILE

--- src/main/java/JDBC/DbConnection.java
+++ src/main/java/JDBC/DbConnection.java
@@ -1,5 +1,24 @@
 package JDBC;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DbConnection {
+
+    public static Connection connection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Akil","root","");
+            System.out.println(connection);
+            return connection;
+        }
+        catch(Exception exception){
+            System.out.println(exception.getMessage());
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+     System.out.println(DbConnection.connection());
+    }
 }
